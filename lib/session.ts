497240b76b6cae706b93e00cd6fb3491b05a89b8
Luis Ortiz <l.ortizintec@gmail.com>
2023-10-26 12:21:07 -0400

    auth setup complete now time to debug

--- lib/session.ts
+++ lib/session.ts
@@ -14,14 +14,22 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-    // jwt: {
-    //     encode: ({ secret, token }) => {
+    jwt: {
+        encode: ({ secret, token }) => {
+            const encodedToken = jsonwebtoken.sign({
+                ...token,
+                iss: 'grafbase',
+                exp: Math.floor(Date.now() / 1000) + 60 * 60
+            }, secret)
 
-    //     },
-    //     decode: async ({ secret, token }) => {
+            return encodedToken;
+        },
+        decode: async ({ secret, token }) => {
+            const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
 
-    //     }
-    // },
+            return decodedToken;
+        }
+    },
     theme: {
         colorScheme: 'light',
         logo: 'logo.png'
@@ -46,8 +54,6 @@ export const authOptions: NextAuthOptions = {
                 console.log('Error retrieving user data: :(', error);
                 return session;
             }
-
-            return session;
         },
         async signIn({ user }: { user: AdapterUser | User }) {
             try {
